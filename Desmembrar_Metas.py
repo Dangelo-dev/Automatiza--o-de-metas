db704f16fab3d9a59991b239b98cbead92b26622
dangelo-dev <victordangelo00@gmail.com>
2024-04-04 12:45:31 -0300

    Script para desmembrar uma planilha de metas usada no serviço em várias outras, considerando em questão os filtros para cada código de loja

---
+++ Desmembrar_Metas.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Alignment, Font, PatternFill
+from openpyxl.utils import get_column_letter
+import tkinter as tk
+from tkinter import filedialog
+
+# Criação de uma janela Tkinter
+root = tk.Tk()
+root.title('Automatização planilha de Metas')
+
+# Função para selecionar o arquivo
+def selecionar_arquivo():
+    arquivo = filedialog.askopenfilename(title="Selecione o arquivo Excel", filetypes=[("Arquivos Excel", "*.xlsx")])
+    if arquivo:
+        processar_arquivo(arquivo)
+        root.destroy()
+    else:
+        print('Nenhum arquivo selecionado.')
+
+# Função principal para processar o arquivo Excel
+def processar_arquivo(arquivo):
+    # Carregamento dos dados das planilhas originais
+    Meta_Original1 = pd.read_excel(arquivo, sheet_name=0, skiprows=1, nrows=16, usecols='A:L')
+    Meta_Original2 = pd.read_excel(arquivo, sheet_name=1, skiprows=3, nrows=17, usecols='A:AH')
+    # Agrupamento dos dados da folha 2 pela coluna 'Cód. Loja'
+    grupos_loja = Meta_Original2.groupby('Cód. Loja')
+    # Iteração sobre os grupos e criação de uma planilha para cada loja (folha 2)
+    for codigo_loja, dados_loja in grupos_loja:
+        criar_planilha_para_loja(Meta_Original1, dados_loja, codigo_loja)
+    print('Processamento concluído. Arquivos de planilha criados.')
+
+# Função para estilizar o cabeçalho
+def style_header(ws):
+    for cell in ws[1]:
+        cell.font = Font(bold=True)
+        cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
+        cell.fill = PatternFill(start_color="FFD700", end_color="FFD700", fill_type="solid")
+
+# Função para criar uma planilha com os dados e estilizar o cabeçalho
+def criar_planilha_para_loja(Meta_Geral, Meta_Diarizada, codigo_loja):
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = f'Meta_Loja_{codigo_loja}'
+    
+    # Adiciona cabeçalho da folha 1
+    ws1.append(list(Meta_Geral.columns))
+    style_header(ws1)
+    for _, row in Meta_Geral.iterrows():
+        if row['Cód. NOVO'] == codigo_loja:
+            ws1.append(row.tolist())  # Adiciona dados da folha 1
+    
+    # Adiciona uma linha em branco
+    ws1.append([])
+
+    # Adiciona o cabeçalho da folha 2 na vertical
+    for col_name in Meta_Diarizada.columns:
+        ws1.append([col_name, Meta_Diarizada.iloc[0][col_name]])
+
+    # Adiciona os dados da folha 2 na vertical
+    for _, row in Meta_Diarizada.iterrows():
+        if row['Cód. Loja'] == codigo_loja:
+            row_data = []
+            for col_name, value in row.items():
+                row_data.append(value)  # Adiciona o valor da linha 5
+                row_data.append("")  # Adiciona um espaço em branco
+            #ws1.append(row_data)  # Adiciona os valores em uma nova linha
+
+    # Centraliza todas as células
+    for row in ws1.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    # Salva a planilha
+    wb.save(f'Meta_Loja_{codigo_loja}.xlsx')
+
+# Chamada da função para selecionar o arquivo
+selecionar_arquivo()
+
+# Execução do loop principal da janela Tkinter
+root.mainloop()
